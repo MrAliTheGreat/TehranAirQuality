253068fd710e96695eb28455a31d379f8923029c
Ali Bahari <alibir80@gmail.com>
2022-02-04 19:08:58 +0330

    Add closing popup plus functions

--- seleniumIntro.py
+++ seleniumIntro.py
@@ -1,25 +1,45 @@
-from datetime import datetime
+from time import sleep
+from matplotlib.pyplot import close
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from datetime import datetime
 import requests
 
+
+def closePopUpWindow(chrome : WebDriver):
+    closeButton = chrome.find_element(
+        By.ID, "GuideMedical"
+    ).find_element(
+        By.CSS_SELECTOR , 'button[aria-label="Close"] > span[aria-hidden="true"]'
+    )
+
+    closeButton.click()
+
+def getImageURL(chrome):
+    return chrome.find_element(
+        By.ID, "imgHeader"
+    ).get_attribute(
+        "src"
+    )
+
+def saveImage(imageURL):
+    photoPath = f'.\Photos\{str(datetime.now()).replace(":" , ",")}.jpg'
+
+    with open(photoPath , 'wb') as handler:
+        handler.write(requests.get(imageURL).content)
+
 options = Options()
-options.headless = True
+options.headless = False
 chrome = webdriver.Chrome(".\chromedriver", options = options)
 
-chrome.implicitly_wait(10)
+chrome.implicitly_wait(5)
 chrome.get("https://airnow.tehran.ir/")
 
-imageURL = chrome.find_element(
-    By.ID, "imgHeader"
-).get_attribute(
-    "src"
-)
-
-photoPath = f'.\Photos\{str(datetime.now()).replace(":" , ",")}.jpg'
-
-with open(photoPath , 'wb') as handler:
-    handler.write(requests.get(imageURL).content)
+closePopUpWindow(chrome)
+imageURL = getImageURL(chrome)
+saveImage(imageURL)
 
 chrome.quit()
